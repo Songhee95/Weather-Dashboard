fb21d737fa9c0b7d3d9124493f7564cc45262d95
songhee <thdgml5537@gmail.com>
2020-10-03 15:28:54 -0400

    When user input city name, the data is stored to local storage and will display after refresh

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+var cityArray = []
+init();
+function init(){
+    JSON.parse(localStorage.getItem('city'));
+}
 function getCity(city){
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7e7ac2e4d0df13d3af84f0a0b1ffdd9b"
     $.ajax({
@@ -8,3 +13,10 @@ function getCity(city){
     })   
 }
 getCity('seoul');
+
+$('button').on('click', function(){
+    var userInput = $('input').val();
+    cityArray.push(userInput);
+    localStorage.setItem('city', JSON.stringify(cityArray));
+    console.log(cityArray);
+})
