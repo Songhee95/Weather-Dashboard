2ec766b134e1eb490bb4bc46b79ad35b19c39c45
songhee <thdgml5537@gmail.com>
2020-10-03 22:48:28 -0400

    get all the input data and get weather information

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,33 @@
-var cityArray = []
-init();
-function init(){
-    JSON.parse(localStorage.getItem('city'));
-}
-function getCity(city){
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7e7ac2e4d0df13d3af84f0a0b1ffdd9b"
-    $.ajax({
-        url : queryURL,
-        method : 'GET' ,
-    }).then(function(response){
-        console.log(response)
-    })   
-}
-getCity('seoul');
-
-$('button').on('click', function(){
-    var userInput = $('input').val();
-    cityArray.push(userInput);
-    localStorage.setItem('city', JSON.stringify(cityArray));
-    console.log(cityArray);
+$(document).ready(function(){
+    var cityArray=[];
+    // get city weather information from API
+    function getCity(city){
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7e7ac2e4d0df13d3af84f0a0b1ffdd9b"
+        $.ajax({
+            url : queryURL,
+            method : 'GET' ,
+        }).then(function(response){
+            console.log(response);
+            // display information from API
+            $('#cityName').text(response.name);
+            $('.lead').append('')
+            response.name;
+            response.main.humidity;
+            response.wind.speed;
+        })   
+    }
+    // get input data from local storage 
+    function init(){
+        var show = JSON.parse(localStorage.getItem('city'));
+        cityArray = show;
+        console.log(cityArray);
+    }
+    // store input data to local storage
+    $('button').on('click', function(){
+        var userInput = $('input').val();
+        getCity(userInput);
+        cityArray.push(userInput);
+        localStorage.setItem('city', JSON.stringify(cityArray));
+    })
+    init();
 })
